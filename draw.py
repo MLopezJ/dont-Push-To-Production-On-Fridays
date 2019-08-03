fdc9e7f54ccbbd86d0403bdbe089be12c7e40e8a
MLopez <lopezjimenezmauro05@gmail.com>
2019-08-03 17:48:06 -0600

    make the drawing with ASCII art

---
+++ draw.py
@@ -0,0 +1,16 @@
+posterUp =   '|￣￣￣￣￣￣￣￣￣￣￣|'
+posterDwon = '|______________________|'
+warningMessage = "     Don't push\n     to production"
+welcomeMessage = "Happy Friday!!" #weekend its almost here
+lineBreak = '\n'
+rabbit = " (\__/) ||\n (•ㅅ•) ||\n/ 　  づ"
+
+def drawing():
+    print(welcomeMessage)
+    print(lineBreak)
+    print(posterUp)
+    print(warningMessage)
+    print(posterDwon)
+    print(rabbit)
+    
+drawing()
