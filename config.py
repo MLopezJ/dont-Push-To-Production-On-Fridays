6aec260a4ec5404114a591f852c05e70d1768fcf
MLopez <lopezjimenezmauro05@gmail.com>
2019-08-03 18:20:07 -0600

    add credentials

---
+++ config.py
@@ -0,0 +1,7 @@
+from os import environ
+
+consumer_key = environ['CONSUMER_KEY']
+consumer_secret = environ['CONSUMER_SECRET']
+
+access_token = environ['ACCESS_KEY']
+access_token_secret = environ['ACCESS_SECRET']
