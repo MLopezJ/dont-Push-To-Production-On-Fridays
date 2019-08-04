56bb075d92493acf76d0536b696eaee96843bd4e
MLopez <lopezjimenezmauro05@gmail.com>
2019-08-03 18:21:45 -0600

    add the file for make the post in Twitter

---
+++ twitter_request.py
@@ -0,0 +1,10 @@
+import tweepy
+
+from config import *
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+api = tweepy.API(auth)
+
+def makePost(message):
+    api.update_status(message)
